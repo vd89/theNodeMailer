998c4200e9943d51eaf4dfa3a1b02ff28ca1110d
vd89 <me@daxcodes.in>
2020-05-20 10:34:18 +0530

    the send mail is workgin and getting the mail

--- Controller/emailer.js
+++ Controller/emailer.js
@@ -1,9 +1,9 @@
-const nodeMailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const config = require('config');
 
-const sendMail = (name,email) => {
+const sendEmail = (name,email) => {
   
-  let transporter = nodeMailer.createTestAccount({
+  let transporter = nodemailer.createTransport({
     host: config.get('emailConfig.host'),
     port: config.get('emailConfig.port'),
     secure: true,
@@ -11,18 +11,23 @@ const sendMail = (name,email) => {
       user: config.get('emailConfig.userName'),
       pass: config.get('emailConfig.password'),
     }
-  })
-
+  });
+  
   let info = transporter.sendMail({
-    from: `"Dax Code Solutions" <${config.get('emailConfig.userName')}>` ,
+    from:`"Dax Code Solutions" <${config.get('emailConfig.userName')}>` ,
     to:`${email}`,
     subject: "The Copy of the feedback form filled on Dax Code Solutions",
-    html: ``,
-  })
+    html: `
+    <h1>Hello , ${name}</h1>
+    <br>
+    <p>Thanks for the feedback form filled by you your feedback is valuable information for yous.</p>
+    `,
+  });
+  console.log("Message sent: %s", info.messageId);
 
 }
 
 
 
 
-module.exports = sendMail
+module.exports = sendEmail
--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const nodemailer = require("nodemailer");
+const sendEmail = require('./Controller/emailer')
 
 const app = express()
 const port = process.env.PORT || 8080
@@ -11,10 +12,21 @@ app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
 app.post('/post',(req,res) => {
-  console.log(req.body);
+  // console.log(req.body);
+  let {name,email} = req.body
+  sendEmail(name,email)
   res.send(`<h1>Thank you </h1> <br> <P> An email has been send to your account  </p> `)
   res.end()
 })
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port} ...... `))
+
+// [Object: null prototype] {
+//   name: 'Dixit Vora',
+//   zipCode: '360005',
+//   email: 'daxcode89@gmail.com',
+//   phone: '+919725625029',
+//   message: 'this is some thext'
+// }
+
