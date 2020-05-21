62740fe5239aa7afa08935bad2403badb0777685
vd89 <me@daxcodes.in>
2020-05-21 11:04:10 +0530

    the change in the then and catch block

--- Controller/emailer.js
+++ Controller/emailer.js
@@ -13,7 +13,7 @@ const sendEmail = (name,email) => {
     }
   });
   
-  let info = transporter.sendMail({
+  transporter.sendMail({
     from:`"Dax Code Solutions" <${config.get('emailConfig.userName')}>` ,
     to:`${email}`,
     subject: "The Copy of the feedback form filled on Dax Code Solutions",
@@ -22,8 +22,13 @@ const sendEmail = (name,email) => {
     <br>
     <p>Thanks for the feedback form filled by you your feedback is valuable information for yous.</p>
     `,
+  }).then((info) => {
+    console.log("Message sent: %s", info.messageId);
+    res.send(`<h1>Thank you </h1> <br> <P> An email has been send to your account  </p> `) 
+  }).catch((err) => {
+    console.log(err);
+    res.send(`<h1> Something wend Wrong and we will get back soon</h1>`)
   });
-  console.log("Message sent: %s", info.messageId);
 
 }
 
--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const nodemailer = require("nodemailer");
 const sendEmail = require('./Controller/emailer')
 
 const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8888
 
 app.use(express.static('View'))
 app.use(express.urlencoded({extended:false}))
@@ -15,18 +15,11 @@ app.post('/post',(req,res) => {
   // console.log(req.body);
   let {name,email} = req.body
   sendEmail(name,email)
-  res.send(`<h1>Thank you </h1> <br> <P> An email has been send to your account  </p> `)
   res.end()
 })
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port} ...... `))
 
-// [Object: null prototype] {
-//   name: 'Dixit Vora',
-//   zipCode: '360005',
-//   email: 'daxcode89@gmail.com',
-//   phone: '+919725625029',
-//   message: 'this is some thext'
-// }
+
 
